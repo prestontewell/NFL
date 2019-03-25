3884e635f50f1d3cac365177fc4e3bfeff2f65d0
prestontewell <preston@tewellphoto.com>
2019-03-25 11:37:09 -0500

    add destroy to teams

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -22,4 +22,21 @@ class Api::TeamsController < ApplicationController
     render 'show.json.jbuilder'
   end
 
+  def update
+    @team = Team.find(params[:id])
+    @team.club = params[:club] || @team.club
+    @team.city = params[:city] || @team.city
+    @team.stadium = params[:stadium] || @team.stadium
+    @team.capacity = params[:capacity] || @team.capacity
+    @team.founded = params[:founded] || @team.founded
+    @team.save
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    render 'destroy.json.jbuilder'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
     get '/teams' => 'teams#index'
     post '/teams' => 'teams#create'
     get '/teams/:id' => 'teams#show'
+    patch '/teams/:id' => 'teams#update'
+    delete '/teams/:id' => 'teams#destroy'
   end
 end
